2d61bf8d8a40a48233c842477c40e83deb3a3a50
EvandroKaibara <yujievandro@gmail.com>
2025-09-22 16:21:26 -0300

    Building project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './assets/logo.svg'
+
+function App() {
+  return (
+    <div className="bg-neutral-background min-h-screen">
+      <aside>
+        <img src={logo} alt="" />
+      </aside>
+    </div>
+  )
+}
+
+export default App
